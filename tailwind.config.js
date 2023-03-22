6688a3390251e55ffaf5d64e36bdc6aaa2f99111
playatanu <playatanu@gmail.com>
2023-03-22 15:34:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#5B5EA6',
+            },
+            backgroundImage: {
+                'hero': "url('images/hero.png')",
+            }
+        }
+    }
+}
